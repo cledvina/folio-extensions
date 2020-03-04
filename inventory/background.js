cbfd0ea7108d15c674e8d7d49d2f38c6f78ae1e5
Charles Ledvina <charles@indexdata.com>
2020-03-04 10:42:32 -0600

    Activate only on inventory full record.

--- inventory/background.js
+++ inventory/background.js
@@ -12,7 +12,7 @@ chrome.runtime.onInstalled.addListener(function() {
 chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
   chrome.declarativeContent.onPageChanged.addRules([{
     conditions: [new chrome.declarativeContent.PageStateMatcher({
-      pageUrl: { hostEquals: 'simmons-test.hosted-folio.indexdata.com' },
+      pageUrl: { urlContains: 'folio.indexdata.com\/inventory\/view\/' },
     })
     ],
     actions: [new chrome.declarativeContent.ShowPageAction()]
